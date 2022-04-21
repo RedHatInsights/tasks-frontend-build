e5894900b9392e512f2e36da99b784fb0c64f226
Travis CI User <travis@example.org>
2022-04-21 17:01:56 +0000

    Update deploy key

---
+++ js/src_SmartComponents_CompletedTasks_CompletedTasks_js.1650560495848.2b42d35b74088575e460.js
@@ -0,0 +1,29 @@
+"use strict";
+(self["webpackChunktasks_frontend"] = self["webpackChunktasks_frontend"] || []).push([["src_SmartComponents_CompletedTasks_CompletedTasks_js"],{
+
+/***/ "./src/SmartComponents/CompletedTasks/CompletedTasks.js":
+/*!**************************************************************!*\
+  !*** ./src/SmartComponents/CompletedTasks/CompletedTasks.js ***!
+  \**************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "webpack/sharing/consume/default/react/react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+
+var CompletedTasks = function CompletedTasks() {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    "aria-label": "completed-tasks"
+  }, "Completed tasks");
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (CompletedTasks);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=../sourcemaps/src_SmartComponents_CompletedTasks_CompletedTasks_js.e9ac2146e51cae49a2ca4b0fab456003.js.map
