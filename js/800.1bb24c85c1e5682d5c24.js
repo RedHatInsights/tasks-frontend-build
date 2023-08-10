17ab300f87bedb37ae859eee7ef37daa7aff0be4
Travis CI User <travis@example.org>
2023-08-10 13:19:08 +0000

    fix inventory permissions (#96)
    Co-authored-by: Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>

---
+++ js/800.1bb24c85c1e5682d5c24.js
@@ -0,0 +1,2 @@
+(self.webpackChunktasks_frontend=self.webpackChunktasks_frontend||[]).push([[800,606],{18446:(n,e,s)=>{var t=s(90939);n.exports=function(n,e){return t(n,e)}},45467:()=>{},11452:()=>{},34946:()=>{},32857:()=>{},28992:()=>{},30187:()=>{},90479:()=>{},78752:()=>{},21626:()=>{},66822:()=>{},54994:()=>{},44690:()=>{},37494:()=>{},67761:()=>{},49854:()=>{}}]);
+//# sourceMappingURL=../sourcemaps/800.97af0cf0961e81f5bb1c41840f6d46d3.js.map
